1c2651628d013145b546442d7fa879c018beb570
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-05-30 12:37:54 +0530

    Added:Swap number

---
+++ Basic__Core__Programs/Basic__CorePrograms/Swap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class Swap
+    {
+        public void SwapCheck(int FirstNumber, int SecondNumber)
+        {
+
+            Console.WriteLine("Before Swap FirstNumber=" + FirstNumber + "SecondNumber=" + SecondNumber);
+            FirstNumber = FirstNumber + SecondNumber;
+            SecondNumber = FirstNumber - SecondNumber;
+            FirstNumber = FirstNumber - SecondNumber;
+            Console.WriteLine("After Swap FirstNumber=" + FirstNumber + "SecondNumber=" + SecondNumber);
+        }
+        }
+}
