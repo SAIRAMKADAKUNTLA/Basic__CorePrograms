55a6ddb5f038805d05db6fff03292c7a02baf3e4
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-05-30 12:30:52 +0530

    Added:finding even or odd

---
+++ Basic__Core__Programs/Basic__CorePrograms/EvenOrOdd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class EvenOrOdd
+    {
+        public void Check()
+        {
+            Console.WriteLine("Enter any Number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            if (Number % 2 == 0)
+                Console.WriteLine("it is a even Number");
+            else
+                Console.WriteLine("it is a odd Number");
+        }
+        }
+}
