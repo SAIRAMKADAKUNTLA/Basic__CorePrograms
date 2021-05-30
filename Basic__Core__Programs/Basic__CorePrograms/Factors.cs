d03049cca4824121cecc815a81824cd347a0cdac
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-05-30 12:31:44 +0530

    Added:finding factors

---
+++ Basic__Core__Programs/Basic__CorePrograms/Factors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class Factors
+    {
+        public void Factor()
+        {
+
+            Console.WriteLine("Enter any Number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= Number; i++)
+            {
+                if (Number % i == 0)
+                {
+                    Console.WriteLine("Factors are" + i);
+
+                }
+            }
+        }
+    }
+}
