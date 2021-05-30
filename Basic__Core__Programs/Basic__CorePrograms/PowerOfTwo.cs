198fb50fc07daa4b34cb481de8a4c5cfd5ab827e
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-05-30 12:37:10 +0530

    Added:finding power of 2

---
+++ Basic__Core__Programs/Basic__CorePrograms/PowerOfTwo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class PowerOfTwo
+    {
+        public void Power()
+        {
+            Console.WriteLine("Enter the Value");
+            int Value = Convert.ToInt32(Console.ReadLine());
+            int Temp = 1;
+            for (int i = 1; i <= Value; i++)
+            {
+                Temp = Temp * 2;
+                Console.WriteLine("Result is:" + Temp);
+            }
+        }
+            }
+}
