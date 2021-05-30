2297c4df13e644b379334e8a4854fda053381f79
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-05-30 12:29:57 +0530

    Added:finging quotient and remainder

---
+++ Basic__Core__Programs/Basic__CorePrograms/Division.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class Division
+    {
+        public void Check(int FirstNumber, int SecondNumber)
+        {
+            int Quotient = FirstNumber / SecondNumber;
+            int Remainder = FirstNumber % SecondNumber;
+            Console.WriteLine("the Quotient is" + Quotient + "the Remainder is" + Remainder);
+        }
+        }
+}
