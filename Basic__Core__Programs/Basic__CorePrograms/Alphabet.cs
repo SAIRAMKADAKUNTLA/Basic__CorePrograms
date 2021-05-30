53aba2659251ce53340202e62df02496596028a1
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-05-30 12:28:22 +0530

    Added:findind vowels and consonents

---
+++ Basic__Core__Programs/Basic__CorePrograms/Alphabet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class Alphabet
+    {
+        public void Check()
+        {
+
+            Console.WriteLine("Enter any Alphabet");
+            int ch = Convert.ToChar(Console.ReadLine());
+            switch (ch)
+            {
+                case 'A':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'E':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'I':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'O':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'U':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                default:
+                    Console.WriteLine("The Alphabet is Consonent");
+                    break;
+            }
+        }
+            }
+}
