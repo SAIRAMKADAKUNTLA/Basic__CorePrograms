5fa9b6d7c6a0810751ab0bbeffd437fb61236ae5
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-05-30 12:35:09 +0530

    Added:Largest number

---
+++ Basic__Core__Programs/Basic__CorePrograms/Largest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class Largest
+    {
+        public void Check(int FirstNumber, int SecondNumber, int ThirdNumber)
+        {
+            if (FirstNumber > SecondNumber && FirstNumber > ThirdNumber)
+                Console.WriteLine("FirstNumber is Largest Number");
+            else if (SecondNumber > ThirdNumber)
+                Console.WriteLine("SecondNumber is Largest");
+            else
+                Console.WriteLine("ThirdNumber is Largest");
+        }
+        }
+}
