4fd22fba0e068ee733791d2d02a981321ebbbbec
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-05-30 12:34:14 +0530

    Added:Harmonic

---
+++ Basic__Core__Programs/Basic__CorePrograms/Harmonic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class Harmonic
+    {
+        public void Check()
+        {
+            Console.WriteLine("Enter the Value");
+            int Value = Convert.ToInt32(Console.ReadLine());
+            int Temp = 1;
+            for (int i = 1; i <= Value; i++)
+            {
+                Temp = Temp + 1 / i;
+                Console.WriteLine("Result is:" + Temp);
+            }
+        }
+            }
+}
