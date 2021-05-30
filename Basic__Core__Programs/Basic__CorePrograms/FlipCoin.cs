46597fb917948e79a37c19d083eab6bc64089721
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-05-30 12:22:50 +0530

    Added:FlipCoin Percentage

---
+++ Basic__Core__Programs/Basic__CorePrograms/FlipCoin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    public class FlipCoin
+    {
+        public void Percentage()
+        {
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check < 1)
+            {
+                Console.WriteLine("Its heads");
+
+            }
+            else
+            {
+                Console.WriteLine("Its tails");
+
+            }
+            float PercentageHeads = (1 * 100 / 1);
+            Console.WriteLine("PercentageHeads of head is:" + PercentageHeads);
+            float PercentageTails = (1 * 100 / 2);
+            Console.WriteLine("Percentage of Tails is:" + PercentageTails);
+        }
+        }
+}
---
+++ Basic__Core__Programs/Basic__CorePrograms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Basic__CorePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Programs");
+           
+
+            }
+    }
+}
